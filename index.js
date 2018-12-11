6882bcbc49d077066907a89523b8e7c7e2278234
danielrhodesy <danielr@outlook.co.nz>
2018-12-12 09:22:36 +1300

    updating project

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const data = require('./data/data.json');
+// const daa = require('./data/bardata.json');
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors());
+
+app.use(function(req, res, next){
+  console.log(`${req.method} request for ${req.url}`);
+  next();
+});
 
 app.get('/data', function(req, res){
   res.json(data);
